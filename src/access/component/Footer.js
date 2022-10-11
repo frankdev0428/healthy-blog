8f7e92710e0af0176a302eaf1ce24c4d58bf9c97
Hoang Le <frankdev0428@gmail.com>
2022-10-11 12:26:28 -0700

    responsive design

--- src/access/component/Footer.js
+++ src/access/component/Footer.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
   render() {
     return (
-      <div className='footer'>
-        <div className='Social-icons'></div>
-        <div className='footer-body'>
-          <div className='Email-input'>
+    <div className='footer'>
+      <div className='header'>
+      <h2 id="logo-title">Health & Fitness</h2>
+      <div className='social-icons'>
+       <a href="#" target="#blank"><i class="fa-brands fa-facebook"></i></a>
+       <a href="#" target="#blank"><i class="fa-brands fa-twitter"></i></a>
+       <a href="#" target="#blank"><i class="fa-brands fa-pinterest"></i></a>
+       <a href="#" target="#blank"><i class="fa-brands fa-instagram"></i></a>
+       <a href="#" target="blank"><i class="fa-brands fa-github"></i></a>
+        </div>
+      </div>
+       <div className='footer-body'>
+          <div className='email-input'>
             <h2>Get our wellness newsletter</h2>
             <p>Filter out the noise and nurture your inbox with health and wellness advice that’s inclusive and rooted in medical expertise.</p>
             <input type="email" placeholder="Enter your email"/>
